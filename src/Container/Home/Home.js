5ee0be018d9f3914431cee0cd0ba0d25710aead5
Jesus de la Fuente <jfuentelucas@gmail.com>
2021-11-20 23:54:19 +0100

    buttons einkommentiert

--- src/Container/Home/Home.js
+++ src/Container/Home/Home.js
@@ -1,34 +1,31 @@
-import React/*  { useEffect} */ from "react";
+import React /*  { useEffect} */ from "react";
 import Typical from "react-typical";
-import Button from "react-bootstrap/Button";
-import "./Home.css";
+import { SiLinkedin, SiGithub } from "react-icons/si";
+import { ImXing } from "react-icons/im";
+/* import Button from "react-bootstrap/Button";
+ */import "./Home.css";
 
 const Home = () => {
-
   /* useEffect(()=>{
-    fetch("https://api.github.com/users/delafuentej/events/public")
-    .then(res => res.json())
-    .then(res =>{
-      console.log('====================================');
+    fetch("https://api.glinkedin===============================');
       console.log(res);
       console.log('====================================');
     })
   },[]) */
 
   return (
-
     <div className="home-container">
+
+      <h1
+      className="home-title">Portfolio under construction</h1>
       <div className="profile-picture">
         <div className="profile-picture-bg"></div>
       </div>
 
       <span className="greeting-text">
         {" "}
-        Hi, I am 
-        <span className="name-text">
-        {" "}
-            Jesús
-        </span>
+        Hi, I am
+        <span className="name-text"> Jesús</span>
       </span>
       <div className="profile-details-role">
         <div className="greeting-text">
@@ -50,29 +47,67 @@ const Home = () => {
           </h1>
         </div>
 
-        <div className="profile-options">
-          <Button 
+        <div className="home-icons">
+          <a
+            href="https://www.linkedin.com/in/jes%C3%BAs-de-la-fuente-b5325047/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="linkedinIcon">
+              <SiLinkedin />
+            </div>
+          </a>
+
+          <a
+            href="https://github.com/delafuentej"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="linkedinIcon">
+              <SiGithub />
+            </div>
+          </a>
+
+          <a
+            href="https://www.xing.com/profile/JESUS_DELAFUENTEDELUCAS2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="xingIcon">
+            <ImXing />
+            </div>
+            
+          </a>
+
+
+          
+          
+          
+        </div>
+
+       {/*   <div className="profile-options">
+          <Button
             variant="dark"
             size="lg"
             style={{
-              margin:"20px auto"
+              margin: "20px auto",
             }}
-          /* className="btn btn-seeProjects" */>
-            
+           
+          >
             My projects{" "}
           </Button>
 
           <Button
-          variant="dark"
-          size="lg"
-          style={{
-            marginLeft:"30px"
-          }}
-           /* className="btn btn-contactMe" */>
+            variant="dark"
+            size="lg"
+            style={{
+              marginLeft: "30px",
+            }}
             
+          >
             Contact Me
           </Button>
-        </div>
+        </div>  */}
       </div>
     </div>
   );
